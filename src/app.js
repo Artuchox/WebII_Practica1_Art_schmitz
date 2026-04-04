358b7cad42da477e4b7a89aedd64606833e41300
Artuchox <160857200+Artuchox@users.noreply.github.com>
2026-04-04 11:57:05 +0200

    implementacion de un app.js basico

---
+++ src/app.js
@@ -0,0 +1,12 @@
+// src/app.js
+import express from 'express'
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/api/health', (req, res) => {
+  res.status(200).json({ status: 'ok' })
+})
+
+export default app
