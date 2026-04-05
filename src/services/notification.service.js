69d6b396c98707b41494f405f7ee55b49170fa62
Artuchox <160857200+Artuchox@users.noreply.github.com>
2026-04-05 16:52:14 +0200

    servicio de notificaciones con EventEmitter
    Implementa NotificationService extendiendo EventEmitter de Node.js.

---
+++ src/services/notification.service.js
@@ -0,0 +1,24 @@
+// src/services/notification.service.js
+import { EventEmitter } from 'node:events'
+
+class NotificationService extends EventEmitter {}
+
+const notificationService = new NotificationService()
+
+notificationService.on('user:registered', (user) => {
+  console.log(`[EVENT] user:registered - ${user.email}`)
+})
+
+notificationService.on('user:verified', (user) => {
+  console.log(`[EVENT] user:verified - ${user.email}`)
+})
+
+notificationService.on('user:invited', (user) => {
+  console.log(`[EVENT] user:invited - ${user.email}`)
+})
+
+notificationService.on('user:deleted', (user) => {
+  console.log(`[EVENT] user:deleted - ${user.email}`)
+})
+
+export default notificationService
