53fb6593e69c74017f463218cb3d73e65f2f0d1c
Artuchox <160857200+Artuchox@users.noreply.github.com>
2026-05-03 16:47:02 +0200

    implementacion de graceful shutdown
    Manejo de señales SIGTERM y SIGINT que cierra ordenadamente Socket.IO, la conexión a MongoDB y el servidor HTTP

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import app from './app.js';
 import env from './config/env.js';
 import dbConnect from './config/db.js';
+import mongoose from 'mongoose'
 
 dbConnect();
 
@@ -9,3 +10,17 @@ app.listen(env.PORT, () => {
     console.log(`Servidor ejecutándose en http://localhost:${env.PORT}`);
     console.log(`Entorno: ${env.NODE_ENV}`);
 });
+
+const shutdown = async (signal) => {
+  console.log(`${signal} recibido, cerrando servidor...`)
+  io.close()
+  await mongoose.connection.close()
+  httpServer.close(() => {
+    console.log('Servidor cerrado correctamente')
+    process.exit(0)
+  })
+}
+ 
+process.on('SIGTERM', () => shutdown('SIGTERM'))
+process.on('SIGINT',  () => shutdown('SIGINT'))
+ 
