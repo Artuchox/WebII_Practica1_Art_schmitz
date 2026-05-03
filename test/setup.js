edea8ddc051c18f1560f0bce5787ca6f8f2319c7
Artuchox <160857200+Artuchox@users.noreply.github.com>
2026-05-03 16:21:51 +0200

    añadir setup de mongodb-memory-server
    Configuración global de Jest con mongodb-memory-server: arranca una instancia en memoria antes de todos los tests, limpia colecciones entre tests y cierra la conexión al finalizar      + eliminacion de de los test creados ya que no lei bien la practica

---
+++ test/setup.js
@@ -0,0 +1,24 @@
+// tests/setup.js
+import { MongoMemoryServer } from 'mongodb-memory-server'
+import mongoose from 'mongoose'
+
+let mongod
+
+beforeAll(async () => {
+  mongod = await MongoMemoryServer.create()
+  const uri = mongod.getUri()
+  await mongoose.connect(uri)
+})
+
+afterAll(async () => {
+  await mongoose.connection.dropDatabase()
+  await mongoose.connection.close()
+  await mongod.stop()
+})
+
+afterEach(async () => {
+  const collections = mongoose.connection.collections
+  for (const key in collections) {
+    await collections[key].deleteMany({})
+  }
+})
